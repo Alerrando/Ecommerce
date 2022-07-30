4be56b88e4d080a02ffd573c6a630903d2281e50
Alerrando Breno <alerrando2@gmail.com>
2022-07-30 20:54:28 -0300

    feat: add style card-info

--- src/components/Carrossel/index.tsx
+++ src/components/Carrossel/index.tsx
@@ -15,10 +15,16 @@ export function Carrossel(){
                         />
                     </div>
 
-                    <div className="z-10 absolute bottom-0 top-[15%] invisible transition duration-300 animação card-info">
-                        <h3>{carrosselImagens.imagem1.title}</h3>
-                        <h4>{carrosselImagens.imagem1.subTitulo}</h4>
-                        <h5>{carrosselImagens.imagem1.desconto}</h5>
+                    <div className="z-10 absolute bottom-0 top-[15%] pl-16 invisible transition duration-300 animação card-info">
+                        <h3 className="text-red-600 text-3xl font-medium">
+                            {carrosselImagens.imagem1.title}
+                        </h3>
+                        <h4 className="text-white text-2xl font-medium mt-1">
+                            {carrosselImagens.imagem1.subTitulo}
+                        </h4>
+                        <h5 className="text-white mt-5">
+                            {carrosselImagens.imagem1.desconto}
+                        </h5>
                     </div>
                 </div>
 
@@ -31,10 +37,16 @@ export function Carrossel(){
                         />
                     </div>
 
-                    <div className="z-10 absolute bottom-0 top-[15%] left-[100%] invisible transition duration-300 animação card-info">
-                        <h3>{carrosselImagens.imagem2.title}</h3>
-                        <h4>{carrosselImagens.imagem2.subTitulo}</h4>
-                        <h5>{carrosselImagens.imagem2.desconto}</h5>
+                    <div className="z-10 absolute bottom-0 top-[15%] left-[100%] pl-16 invisible transition duration-300 animação card-info">
+                        <h3 className="text-red-600 text-3xl font-medium">
+                            {carrosselImagens.imagem2.title}
+                        </h3>
+                        <h4 className="text-white text-2xl font-medium mt-1">
+                            {carrosselImagens.imagem2.subTitulo}
+                        </h4>
+                        <h5 className="text-white mt-5">
+                            {carrosselImagens.imagem2.desconto}
+                        </h5>
                     </div>
                 </div>
 
@@ -47,10 +59,16 @@ export function Carrossel(){
                         />
                     </div>
 
-                    <div className="z-10 absolute bottom-0 top-[15%] left-[200%] invisible transition duration-300 animação card-info">
-                        <h3>{carrosselImagens.imagem3.title}</h3>
-                        <h4>{carrosselImagens.imagem3.subTitulo}</h4>
-                        <h5>{carrosselImagens.imagem3.desconto}</h5>
+                    <div className="z-10 absolute bottom-0 top-[15%] left-[200%] pl-16 invisible transition duration-300 animação card-info">
+                        <h3 className="text-red-600 text-3xl font-medium">
+                            {carrosselImagens.imagem3.title}
+                        </h3>
+                        <h4 className="text-white text-2xl font-medium mt-1 shadow-sm">
+                            {carrosselImagens.imagem3.subTitulo}
+                        </h4>
+                        <h5 className="text-white mt-5">
+                            {carrosselImagens.imagem3.desconto}
+                        </h5>
                     </div>
                 </div>
 
@@ -63,10 +81,16 @@ export function Carrossel(){
                         />
                     </div>
 
-                    <div className="z-10 absolute bottom-0 top-[15%] left-[300%] invisible transition-all duration-300 animação card-info">
-                        <h3>{carrosselImagens.imagem4.title}</h3>
-                        <h4>{carrosselImagens.imagem4.subTitulo}</h4>
-                        <h5>{carrosselImagens.imagem4.desconto}</h5>
+                    <div className="z-10 absolute bottom-0 top-[15%] left-[300%] pl-16 invisible transition-all duration-300 animação card-info">
+                        <h3 className="text-red-600 text-3xl font-medium">
+                            {carrosselImagens.imagem4.title}
+                        </h3>
+                        <h4 className="text-white text-2xl font-medium mt-1">
+                            {carrosselImagens.imagem4.subTitulo}
+                        </h4>
+                        <h5 className="text-white mt-5">
+                            {carrosselImagens.imagem4.desconto}
+                        </h5>
                     </div>
                 </div>
             </div>
