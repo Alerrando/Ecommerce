277b520195791884c75eced562d1ad3099048623
Alerrando Breno <alerrando2@gmail.com>
2022-07-31 17:18:22 -0300

    feat(cards): section cards complete

--- src/components/Cards/index.tsx
+++ src/components/Cards/index.tsx
@@ -1,95 +1,176 @@
-import { cardsInfo } from "../Produtos"
+import { cardsInfo } from "../Produtos";
 
-export function Cards(){
-    const Fileira1 = cardsInfo.fileira1
+export function Cards() {
+  const Fileira1 = cardsInfo.fileira1;
+  const Fileira2 = cardsInfo.fileira2;
 
-    return(
-        <>
-            <div>
-                <div className="alinhamento justify-around mt-12">
-                    <div className="w-72 h-[30rem] relative">
-                        <div className="w-full h-full">
-                            <img 
-                                src={Fileira1.card1.url} 
-                                alt={Fileira1.card1.subTitulo}
-                                className="w-full h-full rounded-xl"
-                            />
-                        </div>
-
-                        <div className="cards-info absolute bottom-0 p-8">
-                            <h3>{Fileira1.card1.titulo}</h3>
-                            <h4>{Fileira1.card1.subTitulo}</h4>
-                            <h5>{Fileira1.card1.descrição}</h5>
-                        </div>
-                    </div>
-
-                    <div className="w-72 h-[30rem] rounded-2xl relative">
-                        <div className="w-full h-full">
-                            <img 
-                                src={Fileira1.card2.url} 
-                                alt={Fileira1.card2.subTitulo}
-                                className="w-full h-full rounded-xl"
-                            />
-                        </div>
-
-                        <div className="cards-info absolute bottom-0 p-8">
-                            <h3>{Fileira1.card2.titulo}</h3>
-                            <h4>{Fileira1.card2.subTitulo}</h4>
-                            <h5>{Fileira1.card2.descrição}</h5>
-                        </div>
-                    </div>
-
-                    <div className="w-72 h-[30rem] rounded-2xl relative">
-                        <div className="w-full h-full">
-                            <img 
-                                src={Fileira1.card3.url} 
-                                alt={Fileira1.card3.subTitulo}
-                                className="w-full h-full rounded-xl"
-                            />
-                        </div>
-
-                        <div className="cards-info absolute bottom-0 p-8">
-                            <h3>{Fileira1.card3.titulo}</h3>
-                            <h4>{Fileira1.card3.subTitulo}</h4>
-                            <h5>{Fileira1.card3.descrição}</h5>
-                        </div>
-                    </div>
-                </div>
+  return (
+    <>
+      <div>
+        <div className="alinhamento justify-evenly pt-6 pb-[4.5rem]">
+          <div className="w-80 h-[36rem] relative text-center group">
+            <div className="w-full h-full">
+              <img
+                src={Fileira1.card1.url}
+                alt={Fileira1.card1.subTitulo}
+                className="w-full h-full rounded-xl"
+              />
             </div>
 
-            <div>
-                <div className="alinhamento justify-around mt-12">
-                    <div className="w-60 h-[30rem] rounded-2xl bg-red-500">
-                        <div>
-
-                        </div>
-
-                        <div className="cards-info">
-
-                        </div>
-                    </div>
-
-                    <div className="w-60 h-[30rem] rounded-2xl bg-red-500">
-                        <div>
-
-                        </div>
-
-                        <div className="cards-info">
-
-                        </div>
-                    </div>
-
-                    <div className="w-60 h-[30rem] rounded-2xl bg-red-500">
-                        <div>
-
-                        </div>
-
-                        <div className="cards-info">
-
-                        </div>
-                    </div>
-                </div>
+            <div className="absolute invisible bottom-0 p-8 text-start group-hover:visible group-hover:pb-12 transition-all">
+              <h3 className="text-red-500 font-Rokkitt font-medium text-3xl">
+                {Fileira1.card1.titulo}
+              </h3>
+              <h4 className="text-white font-Rokkitt font-medium text-2xl">
+                {Fileira1.card1.subTitulo}
+              </h4>
+              <h5 className="text-white mt-6 text-sm">
+                {Fileira1.card1.descrição}
+              </h5>
             </div>
-        </>
-    )
+
+            <button className="text-white bg-zinc-600 px-4 py-1 mt-4 hover:bg-blue-900 transition-colors">
+                Adicionar ao carrinho
+            </button>
+          </div>
+
+          <div className="w-80 h-[36rem] rounded-2xl relative text-center group">
+            <div className="w-full h-full">
+              <img
+                src={Fileira1.card2.url}
+                alt={Fileira1.card2.subTitulo}
+                className="w-full h-full rounded-xl"
+              />
+            </div>
+
+            <div className="absolute invisible bottom-0 p-8 text-start group-hover:visible group-hover:pb-12 transition-all">
+              <h3 className="text-red-500 font-Rokkitt font-medium text-3xl">
+                {Fileira1.card2.titulo}
+              </h3>
+              <h4 className="text-white font-Rokkitt font-medium text-2xl">
+                {Fileira1.card2.subTitulo}
+              </h4>
+              <h5 className="text-white mt-6 text-sm">
+                {Fileira1.card2.descrição}
+              </h5>
+            </div>
+
+            <button className="text-white bg-zinc-600 px-4 py-1 mt-4 hover:bg-blue-900 transition-colors">
+                Adicionar ao carrinho
+            </button>
+          </div>
+
+          <div className="w-80 h-[36rem] rounded-2xl relative text-center group">
+            <div className="w-full h-full">
+              <img
+                src={Fileira1.card3.url}
+                alt={Fileira1.card3.subTitulo}
+                className="w-full h-full rounded-xl"
+              />
+            </div>
+
+            <div className="absolute invisible bottom-0 p-8 text-start group-hover:visible group-hover:pb-12 transition-all">
+              <h3 className="text-red-500 font-Rokkitt font-medium text-3xl">
+                {Fileira1.card3.titulo}
+              </h3>
+              <h4 className="text-white font-Rokkitt font-medium text-2xl">
+                {Fileira1.card3.subTitulo}
+              </h4>
+              <h5 className="text-white mt-6 text-sm">
+                {Fileira1.card3.descrição}
+              </h5>
+            </div>
+
+            <button className="text-white bg-zinc-600 px-4 py-1 mt-4 hover:bg-blue-900 transition-colors">
+                Adicionar ao carrinho
+            </button>
+          </div>
+        </div>
+
+        <div className="alinhamento justify-evenly pt-6 pb-[4.5rem]">
+          <div className="w-80 h-[36rem] relative text-center group">
+            <div className="w-full h-full">
+              <img
+                src={Fileira2.card1.url}
+                alt={Fileira2.card1.subTitulo}
+                className="w-full h-full rounded-xl"
+              />
+            </div>
+
+            <div className="absolute invisible bottom-0 p-8 text-start group-hover:visible group-hover:pb-12 transition-all">
+              <h3 className="text-red-500 font-Rokkitt font-medium text-3xl">
+                {Fileira2.card1.titulo}
+              </h3>
+              <h4 className="text-white font-Rokkitt font-medium text-2xl">
+                {Fileira2.card1.subTitulo}
+              </h4>
+              <h5 className="text-white mt-6 text-sm">
+                {Fileira2.card1.descrição}
+              </h5>
+            </div>
+
+            <button className="text-white bg-zinc-600 px-4 py-1 mt-4 hover:bg-blue-900 transition-colors">
+                Adicionar ao carrinho
+            </button>
+          </div>
+
+          <div className="w-80 h-[36rem] rounded-2xl relative text-center group">
+            <div className="w-full h-full">
+              <img
+                src={Fileira2.card2.url}
+                alt={Fileira2.card2.subTitulo}
+                className="w-full h-full rounded-xl"
+              />
+            </div>
+
+            <div className="absolute invisible bottom-0 p-8 text-start group-hover:visible group-hover:pb-12 transition-all">
+              <h3 className="text-red-500 font-Rokkitt font-medium text-3xl">
+                {Fileira2.card2.titulo}
+              </h3>
+              <h4 className="text-white font-Rokkitt font-medium text-2xl">
+                {Fileira2.card2.subTitulo}
+              </h4>
+              <h5 className="text-white mt-6 text-sm">
+                {Fileira2.card2.descrição}
+              </h5>
+            </div>
+
+            <button className="text-white bg-zinc-600 px-4 py-1 mt-4 hover:bg-blue-900 transition-colors">
+                Adicionar ao carrinho
+            </button>
+          </div>
+
+          <div className="w-80 h-[36rem] rounded-2xl relative text-center group">
+            <div className="w-full h-full">
+              <img
+                src={Fileira2.card3.url}
+                alt={Fileira2.card3.subTitulo}
+                className="w-full h-full rounded-xl"
+              />
+            </div>
+
+            <div className="absolute invisible bottom-0 p-8 text-start group-hover:visible group-hover:pb-12 transition-all">
+              <h3 className="text-red-500 font-Rokkitt font-medium text-3xl">
+                {Fileira2.card3.titulo}
+              </h3>
+              <h4 className="text-white font-Rokkitt font-medium text-2xl">
+                {Fileira2.card3.subTitulo}
+              </h4>
+              <h5 className="text-white mt-6 text-sm">
+                {Fileira2.card3.descrição}
+              </h5>
+            </div>
+
+            <button className="text-white bg-zinc-600 px-4 py-1 mt-4 hover:bg-blue-900 transition-colors">
+                Adicionar ao carrinho
+            </button>
+          </div>
+        </div>
+
+        
+      </div>
+
+      
+    </>
+  );
 }
--- src/components/Produtos/index.tsx
+++ src/components/Produtos/index.tsx
@@ -4,7 +4,7 @@ import { Titulo } from "../Titulo";
 export const cardsInfo = {
     fileira1: {
         card1: {
-            url: "https://images.unsplash.com/photo-1598327105666-5b89351aff97?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=327&q=80",
+            url: "https://images.unsplash.com/photo-1598327106026-d9521da673d1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=327&q=80",
             titulo: "Iphone",
             subTitulo: "Pro Max",
             descrição : "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod qui delectus repudiandae eum? \
@@ -25,6 +25,35 @@ export const cardsInfo = {
             url: "https://images.unsplash.com/photo-1543854651-42d750ab10b2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=399&q=80",
             titulo: "Camera",
             subTitulo: "Clicking Camera",
+            descrição : "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod qui delectus repudiandae eum? \
+            Tempora rem aliquam, quo aut repellat omnis laboriosam molestiae error qui magnam quae suscipit sint esse. \
+            Commodi?",
+        },
+    },
+
+    fileira2: {
+        card1: {
+            url: "https://images.unsplash.com/photo-1565630916779-e303be97b6f5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80",
+            titulo: "Notebook",
+            subTitulo: "Dell Inspiron 3000",
+            descrição : "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod qui delectus repudiandae eum? \
+            Tempora rem aliquam, quo aut repellat omnis laboriosam molestiae error qui magnam quae suscipit sint esse. \
+            Commodi?",
+        },
+
+        card2: {
+            url: "https://images.unsplash.com/photo-1544244015-9c72fd9c866d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=382&q=80",
+            titulo: "iPad",
+            subTitulo: "Pro 2018",
+            descrição : "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod qui delectus repudiandae eum? \
+            Tempora rem aliquam, quo aut repellat omnis laboriosam molestiae error qui magnam quae suscipit sint esse. \
+            Commodi?",
+        },
+
+        card3: {
+            url: "https://images.unsplash.com/photo-1602837385569-08ac19ec83af?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=326&q=80",
+            titulo: "Hardware",
+            subTitulo: "Core i9",
             descrição : "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod qui delectus repudiandae eum? \
             Tempora rem aliquam, quo aut repellat omnis laboriosam molestiae error qui magnam quae suscipit sint esse. \
             Commodi?",
