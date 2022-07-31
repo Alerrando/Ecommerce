36f013f0d1cb37947591d9a8bc50facc1aaa98d0
Alerrando Breno <alerrando2@gmail.com>
2022-07-31 15:44:20 -0300

    feat(carrossel): adating cards-info

--- src/components/Cards/index.tsx
+++ src/components/Cards/index.tsx
@@ -1,36 +1,95 @@
+import { cardsInfo } from "../Produtos"
+
 export function Cards(){
+    const Fileira1 = cardsInfo.fileira1
+
     return(
         <>
             <div>
                 <div className="alinhamento justify-around mt-12">
-                    <div className="w-60 h-[28rem] rounded-2xl bg-red-500">
+                    <div className="w-72 h-[30rem] relative">
+                        <div className="w-full h-full">
+                            <img 
+                                src={Fileira1.card1.url} 
+                                alt={Fileira1.card1.subTitulo}
+                                className="w-full h-full rounded-xl"
+                            />
+                        </div>
 
+                        <div className="cards-info absolute bottom-0 p-8">
+                            <h3>{Fileira1.card1.titulo}</h3>
+                            <h4>{Fileira1.card1.subTitulo}</h4>
+                            <h5>{Fileira1.card1.descrição}</h5>
+                        </div>
                     </div>
-                    <div className="w-60 h-[28rem] rounded-2xl bg-red-500">
 
+                    <div className="w-72 h-[30rem] rounded-2xl relative">
+                        <div className="w-full h-full">
+                            <img 
+                                src={Fileira1.card2.url} 
+                                alt={Fileira1.card2.subTitulo}
+                                className="w-full h-full rounded-xl"
+                            />
+                        </div>
+
+                        <div className="cards-info absolute bottom-0 p-8">
+                            <h3>{Fileira1.card2.titulo}</h3>
+                            <h4>{Fileira1.card2.subTitulo}</h4>
+                            <h5>{Fileira1.card2.descrição}</h5>
+                        </div>
                     </div>
-                    <div className="w-60 h-[28rem] rounded-2xl bg-red-500">
 
+                    <div className="w-72 h-[30rem] rounded-2xl relative">
+                        <div className="w-full h-full">
+                            <img 
+                                src={Fileira1.card3.url} 
+                                alt={Fileira1.card3.subTitulo}
+                                className="w-full h-full rounded-xl"
+                            />
+                        </div>
+
+                        <div className="cards-info absolute bottom-0 p-8">
+                            <h3>{Fileira1.card3.titulo}</h3>
+                            <h4>{Fileira1.card3.subTitulo}</h4>
+                            <h5>{Fileira1.card3.descrição}</h5>
+                        </div>
                     </div>
                 </div>
-                
             </div>
 
             <div>
                 <div className="alinhamento justify-around mt-12">
-                    <div className="w-60 h-[28rem] rounded-2xl bg-red-500">
+                    <div className="w-60 h-[30rem] rounded-2xl bg-red-500">
+                        <div>
 
+                        </div>
+
+                        <div className="cards-info">
+
+                        </div>
                     </div>
-                    <div className="w-60 h-[28rem] rounded-2xl bg-red-500">
 
+                    <div className="w-60 h-[30rem] rounded-2xl bg-red-500">
+                        <div>
+
+                        </div>
+
+                        <div className="cards-info">
+
+                        </div>
                     </div>
-                    <div className="w-60 h-[28rem] rounded-2xl bg-red-500">
 
+                    <div className="w-60 h-[30rem] rounded-2xl bg-red-500">
+                        <div>
+
+                        </div>
+
+                        <div className="cards-info">
+
+                        </div>
                     </div>
                 </div>
-                
             </div>
-        
         </>
     )
 }
--- src/components/Produtos/index.tsx
+++ src/components/Produtos/index.tsx
@@ -1,6 +1,37 @@
 import { Cards } from "../Cards";
 import { Titulo } from "../Titulo";
 
+export const cardsInfo = {
+    fileira1: {
+        card1: {
+            url: "https://images.unsplash.com/photo-1598327105666-5b89351aff97?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=327&q=80",
+            titulo: "Iphone",
+            subTitulo: "Pro Max",
+            descrição : "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod qui delectus repudiandae eum? \
+            Tempora rem aliquam, quo aut repellat omnis laboriosam molestiae error qui magnam quae suscipit sint esse. \
+            Commodi?",
+        },
+
+        card2: {
+            url: "https://images.unsplash.com/photo-1598717873846-0418a74950f5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=327&q=80",
+            titulo: "Apple",
+            subTitulo: "Watch",
+            descrição : "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod qui delectus repudiandae eum? \
+            Tempora rem aliquam, quo aut repellat omnis laboriosam molestiae error qui magnam quae suscipit sint esse. \
+            Commodi?",
+        },
+
+        card3: {
+            url: "https://images.unsplash.com/photo-1543854651-42d750ab10b2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=399&q=80",
+            titulo: "Camera",
+            subTitulo: "Clicking Camera",
+            descrição : "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod qui delectus repudiandae eum? \
+            Tempora rem aliquam, quo aut repellat omnis laboriosam molestiae error qui magnam quae suscipit sint esse. \
+            Commodi?",
+        },
+    }
+}
+
 export function Produtos(){
     return (
         <div>
