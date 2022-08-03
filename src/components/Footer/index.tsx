8cea117a8024b5ed14c12d778633cc52235532fb
Alerrando Breno <alerrando2@gmail.com>
2022-08-03 16:57:13 -0300

    footer structure made

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,6 +1,101 @@
-export function Footer(){
-    return(
-        <footer>
-        </footer>
-    )
+export function Footer() {
+  return (
+    <footer className="mt-32">
+      <div className="footer-grid">
+        <div className="footer-info">
+          <div>
+            <h3 className="">Sobre o Site</h3>
+          </div>
+
+          <div>
+            <p className="">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit
+              odit magni id veritatis natus fugiat facilis perferendis aliquid.
+            </p>
+          </div>
+        </div>
+
+        <div className="footer-info">
+          <div>
+            <h3 className="">Serviço de Atendimento</h3>
+          </div>
+          <ul>
+            <p className="">
+              <a href="">Contato</a>
+            </p>
+            <p className="">
+              <a href="">Reembolso/Troca</a>
+            </p>
+            <p className="">
+              <a href="">Codigo de Desconto</a>
+            </p>
+            <p className="">
+              <a href="">Lista de Desejos</a>
+            </p>
+            <p className="">
+              <a href="">Produtos Especiais</a>
+            </p>
+            <p className="">
+              <a href="">Mapa do Site</a>
+            </p>
+          </ul>
+        </div>
+
+        <div className="footer-info">
+          <div>
+            <h3 className="">Informações</h3>
+          </div>
+          <ul>
+            <p className="">
+              <a href="">Sobre nos</a>
+            </p>
+            <p className="">
+              <a href="">Entrega</a>
+            </p>
+            <p className="">
+              <a href="">Política de Privacidade</a>
+            </p>
+            <p className="">
+              <a href="">Suporte</a>
+            </p>
+            <p className="text-lg font-Rokkitt opacity-70 hover:opacity-100 transition-colors">
+              <a href="">Rastreamento de Pedido</a>
+            </p>
+          </ul>
+        </div>
+
+        <div className="footer-info">
+          <div>
+            <h3 className="">Criador</h3>
+          </div>
+
+          <ul>
+            <p className="">
+              <a href="">Alerrando</a>
+            </p>
+          </ul>
+        </div>
+
+        <div className="footer-info">
+          <div>
+            <h3 className="">Contatos</h3>
+          </div>
+          <ul>
+            <p className="">
+              291 South 21th Street, Suite 721 New York
+            </p>
+            <p className="">
+              <a href="">(18) 1234-5678</a>
+            </p>
+            <p className="">
+              <a href="">info@seusite.com</a>
+            </p>
+            <p className="">
+              <a href="">seusite.com</a>
+            </p>
+          </ul>
+        </div>
+      </div>
+    </footer>
+  );
 }
