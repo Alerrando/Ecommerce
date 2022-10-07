98fd5d99f8af9c8670c028e0692ef6d83d9699c8
Alerrando Breno <alerrando2@gmail.com>
2022-10-07 13:21:13 -0300

    titulo para mobile

--- src/components/Titulo.tsx
+++ src/components/Titulo.tsx
@@ -1,11 +1,13 @@
+import React from "react";
+
 type TituloProps = {
     name: string;
 }
 
 export function Titulo(props: TituloProps){
     return (
-        <div className="w-full h-16 bg-[#88c8bc] alinhamento justify-center">
-            <h2 className="text-3xl text-white font-Rokkitt">{props.name}</h2>
+        <div className="w-full h-12 md:h-16 bg-[#88c8bc] alinhamento justify-center">
+            <h2 className="text-2xl md:text-3xl text-white font-Rokkitt">{props.name}</h2>
         </div>
     )
 }
