ee5b88dd91885c0c98835f6aff0cdaadcacf224c
Alerrando Breno <alerrando2@gmail.com>
2022-10-08 15:37:35 -0300

    ajustes no carrossel

--- src/components/Carrossel/index.tsx
+++ src/components/Carrossel/index.tsx
@@ -8,7 +8,7 @@ export function Carrossel() {
         {carrosselImagens.map((imagem, index) => (
           <>
             <div className="w-full relative promoção">
-              <div className="w-[27rem] md:w-[1735px] flex h-full animação">
+              <div className="w-[27rem] md:w-[79rem] flex h-full animação">
                 <img
                   src={carrosselImagens[index].url}
                   alt={carrosselImagens[index].subTitulo}
