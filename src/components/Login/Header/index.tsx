d0dd63afc27a12bc21d933ab9711a5c8425b6ebd
Alerrando Breno <alerrando2@gmail.com>
2022-12-13 16:42:20 -0300

    create header login

---
+++ src/components/Login/Header/index.tsx
@@ -0,0 +1,12 @@
+// @flow 
+import * as React from 'react';
+type Props = {
+    
+};
+export function Header(props: Props){
+    return (
+        <div>
+            Hello World
+        </div>
+    );
+};
--- src/components/Login/index.tsx
+++ src/components/Login/index.tsx
@@ -1,9 +1,11 @@
 import { Main } from "./Main";
 import React from 'react';
+import { Header } from "./Header";
 
 export function Login(){
     return(
         <>
+            <Header />
             <Main />        
         </>
     )
