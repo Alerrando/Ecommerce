b05fa4ab1e1c91c38da402c00a2c5390492ba642
Alerrando Breno <alerrando2@gmail.com>
2022-12-14 13:10:54 -0300

    structure header page login

--- src/components/Login/Header/index.tsx
+++ src/components/Login/Header/index.tsx
@@ -1,12 +1,13 @@
-// @flow 
-import * as React from 'react';
-type Props = {
-    
-};
-export function Header(props: Props){
-    return (
-        <div>
-            Hello World
-        </div>
-    );
-};
+// @flow
+import * as React from "react";
+import { ArrowLeft } from 'phosphor-react';
+
+export function Header() {
+  return (
+    <header className="w-full h-auto">
+      <div className="flex items-center justify-between py-2 px-6">
+        
+      </div>
+    </header>
+  );
+}
--- src/components/Login/Main/index.tsx
+++ src/components/Login/Main/index.tsx
@@ -1,8 +1,9 @@
 import React from "react";
 
 export function Main() {
+  
   return (
-    <main className="w-full h-screen flex items-center justify-center pt-[64px] text-white">
+    <main className="w-full h-[90vh] flex items-center justify-center text-white">
       <div className="w-[380px] md:w-[46%] h-[500px] md:h-auto bg-white overflow-auto rounded-2xl border border-zinc-600 shadow-lg">
         <div className="grid gap-4 px-6 py-6">
           <header className="w-full h-auto text-black" translate="no">
