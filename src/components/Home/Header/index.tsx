919ba0468254680a516a17073f5a47fdaa853d64
Alerrando Breno <alerrando2@gmail.com>
2022-12-14 14:15:10 -0300

    add path to login in responsive

--- src/components/Home/Header/index.tsx
+++ src/components/Home/Header/index.tsx
@@ -39,7 +39,7 @@ export function Header() {
           </button>
         </div>
 
-        <div className="text-center gap-8 hidden md:flex">
+        <div className="text-center gap-8 flex absolute top-[13%] md:relative">
           <ShoppingCart size={24} weight="bold" />
           <Link to="/login">Login</Link>
         </div>
