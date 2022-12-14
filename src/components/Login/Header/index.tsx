95c4229c39035ad41c68937237118f159c2dce82
Alerrando Breno <alerrando2@gmail.com>
2022-12-14 13:13:20 -0300

    login page header elements

--- src/components/Login/Header/index.tsx
+++ src/components/Login/Header/index.tsx
@@ -6,7 +6,13 @@ export function Header() {
   return (
     <header className="w-full h-auto">
       <div className="flex items-center justify-between py-2 px-6">
-        
+        <ArrowLeft size={32} />
+
+        <h1 className="text-4xl font-Playfair-Display text-[#474747] font-semibold">
+          TechSoft
+        </h1>
+
+        <div></div>
       </div>
     </header>
   );
