9c6d6c6d844478f60ef012ce634c6db45ed81d3e
Alerrando Breno <alerrando2@gmail.com>
2022-12-15 15:07:25 -0300

    add router register

--- src/Router.tsx
+++ src/Router.tsx
@@ -2,6 +2,7 @@ import * as React from 'react';
 import { Route, Routes, BrowserRouter } from "react-router-dom";
 import { App } from "./App";
 import { Login } from "./components/Login";
+import { Register } from './components/Register';
 
 
 export function Router(){
@@ -10,6 +11,7 @@ export function Router(){
             <Routes>
                     <Route path="/" element={<App />} />
                     <Route path="/login" element={<Login />} />
+                    <Route path="/register" element={<Register /> } />
             </Routes>
         </BrowserRouter>
     )
