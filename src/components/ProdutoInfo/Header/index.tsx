242e0ce0afde02e7d809795fe9f3495c90b3e369
Alerrando Breno <alerrando2@gmail.com>
2022-12-18 15:06:50 -0300

    add route back to home

--- src/components/ProdutoInfo/Header/index.tsx
+++ src/components/ProdutoInfo/Header/index.tsx
@@ -1,12 +1,15 @@
 // @flow 
 import * as React from 'react';
+import { Link } from 'react-router-dom';
 type Props = {
     
 };
 export function Header(props: Props){
     return (
-        <header>
-            <h1>Hello World</h1>
+        <header className='w-full h-auto flex items-center justify-center border-b border-b-zinc-300'>
+            <div className='w-[85%] h-full py-3'>
+                <Link to="/" className='text-2xl font-Playfair-Display text-[#474747] font-semibold'>TechSoft</Link>
+            </div>
         </header>
     );
 };
