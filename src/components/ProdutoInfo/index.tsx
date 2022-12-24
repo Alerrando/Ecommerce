a17123f4bc59b02de7ef7dd749542f3fb74b0279
Alerrando Breno <alerrando2@gmail.com>
2022-12-24 00:44:31 -0300

    fixing bugs

--- src/components/ProdutoInfo/index.tsx
+++ src/components/ProdutoInfo/index.tsx
@@ -1,14 +1,12 @@
 // @flow
 import * as React from "react";
-import { useParams } from "react-router-dom";
+import { useParams, useRoutes } from "react-router-dom";
 import { getProduto } from "../../utils/getProduto";
 import { Header } from "./Header";
 import { Main } from "./Main";
 
 export function ProdutoInfo() {
-  const { id } = useParams();
-  const produto = getProduto(id as string);
-
+  const { id } = useParams();  
   
 
   return (
