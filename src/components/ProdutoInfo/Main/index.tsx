ae0fed8cae02c3a32bb3020e11ef5c9abe23228e
Alerrando Breno <alerrando2@gmail.com>
2022-12-25 01:42:52 -0300

    add image thumbnail

--- src/components/ProdutoInfo/Main/index.tsx
+++ src/components/ProdutoInfo/Main/index.tsx
@@ -1,5 +1,7 @@
 // @flow 
+import { HouseSimple } from 'phosphor-react';
 import * as React from 'react';
+import { Link } from 'react-router-dom';
 type MainProps = {
     produto: any;
 };
@@ -9,10 +11,23 @@ export function Main(props: MainProps){
     return (
         <main className='w-full h-full'>
             <div className='py-4 px-[6rem] grid gap-4'>
-                <p className='text-lg font-medium'>{`${produto.titulo} ${produto.subTitulo}`}</p>
+                <div className='w-[32.5rem] flex items-center gap-6'>
+                    <Link to="/">
+                        <HouseSimple size={22} />                    
+                    </Link>
+                    <span>{">"}</span>
+                    <p className='text-lg font-medium cursor-pointer hover:underline'>{`${produto.titulo} ${produto.subTitulo}`}</p>
 
-                <div className='w-[350px] h-[520px]'>
-                    <img src={`/public/${produto.img}`} alt={produto.url} className="w-full h-full bg-cover" />
+                </div>
+                
+                <div className='flex gap-6'>
+                    <div className='grid gap-8'>
+                        <img src={`${produto.img}`} alt={produto.url} className="w-32 h-32 bg-cover"  />
+                    </div>
+
+                    <div className='w-[21.875rem] h-[32.5rem]'>
+                        <img src={`${produto.img}`} alt={produto.url} className="w-full h-full bg-cover" />
+                    </div>
                 </div>
             </div>
         </main>
