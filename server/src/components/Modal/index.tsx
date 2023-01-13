8ca7281f464f29fd15a8b8ea4e682128f3d06b4c
Alerrando <alerrando2@gmail.com>
2023-01-13 09:34:12 -0300

    add modal close

--- server/src/components/Modal/index.tsx
+++ server/src/components/Modal/index.tsx
@@ -1,10 +1,17 @@
+import { X } from 'phosphor-react'
 import React from 'react'
+import { ShopContext } from '../../context/shopContext'
 
 export function Modal(){
-    return (
-        <div className='w-full h-screen fixed flex items-center justify-center top-0 left-0 bg-sombreamento'>
-            <div className='w-2/3 h-3/4 bg-white'>
+    const { modalInfo, setModalInfo } = React.useContext(ShopContext)
 
+ 
+    return (
+        <div className='w-full h-screen fixed flex items-center justify-center top-0 left-0 bg-sombreamento z-50'>
+            <div className='w-2/3 h-3/4 bg-white'>
+                <header className='py-2 px-8'>
+                    <X size={32} className="cursor-pointer" onClick={() => setModalInfo({})} />
+                </header>
             </div>
         </div>
     )
