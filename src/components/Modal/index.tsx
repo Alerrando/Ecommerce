6dbc282106848c800df6b6d1ff71debbfa28f220
Alerrando <alerrando2@gmail.com>
2023-01-15 22:59:38 -0300

    add image and description in modal

--- src/components/Modal/index.tsx
+++ src/components/Modal/index.tsx
@@ -1,17 +1,29 @@
 import { X } from 'phosphor-react'
 import React from 'react'
-import { ShopContext } from '../../context/shopContext'
+import { CardKeys, ShopContext } from '../../context/shopContext'
 
 export function Modal(){
-    const { modalInfo, setModalInfo } = React.useContext(ShopContext)
+    const { modalInfo, setModalInfo } = React.useContext(ShopContext);
+
+    console.log(modalInfo)
 
  
     return (
         <div className='w-full h-screen fixed flex items-center justify-center top-0 left-0 bg-sombreamento z-50'>
-            <div className='w-2/3 h-3/4 bg-white'>
-                <header className='py-2 px-8'>
-                    <X size={32} className="cursor-pointer" onClick={() => setModalInfo({})} />
+            <div className='w-2/3 h-3/4 px-8 bg-white'>
+                <header className='py-2 flex items-center justify-end'>
+                    <X size={26} className="cursor-pointer" onClick={() => setModalInfo({} as CardKeys)} />
                 </header>
+
+                <section className='w-full h-full flex'>
+                    <div className='h-full max-h-[86%] min-w-[24rem] py-4 border border-gray-700 border-solid shadow-lg'>
+                        <img src={modalInfo.image} alt={modalInfo.url} className="w-full h-full object-cover" />
+                    </div>
+
+                    <div className='grid gap-3 px-6'>
+                        <h2 className='text-xl'>{modalInfo.descricao}</h2>
+                    </div>
+                </section>
             </div>
         </div>
     )
--- src/context/shopContext.tsx
+++ src/context/shopContext.tsx
@@ -23,6 +23,7 @@ export type CardKeys = {
     categoria: string;
     destaque: number;
     imageDestaque: string;
+    descricao: string;
 };
 
 type ShopContextProps = {
