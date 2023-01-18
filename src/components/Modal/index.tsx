64845a98b28ce6909bf325949832c6430ae44b76
Alerrando <alerrando2@gmail.com>
2023-01-18 01:11:45 -0300

    improving code

--- src/components/Modal/index.tsx
+++ src/components/Modal/index.tsx
@@ -10,17 +10,15 @@ export function Modal(){
  
     return (
         <div className='w-full h-screen fixed flex items-center justify-center top-0 left-0 bg-sombreamento z-50'>
-            <div className='w-2/3 h-3/4 px-0 sm:px-8 bg-white overflow-auto sm:overflow-hidden'>
+            <div className='w-2/3 h-3/4 px-0 sm:pl-8 bg-white overflow-auto sm:overflow-hidden'>
                 <header className='py-2 flex items-center justify-end'>
                     <X size={26} className="cursor-pointer" onClick={() => setModalInfo({} as CardKeys)} />
                 </header>
 
-                <section className='w-full h-full block sm:flex'>
-                    <div className='h-full w-full max-h-[86%] min-w-[240px] md:min-w-[60%] py-4'>
-                        <img src={modalInfo.image} alt={modalInfo.url} className="w-full h-full object-cover" />
-                    </div>
+                <section className='w-full h-[85%] block sm:flex'>
+                    <img src={modalInfo.image} alt={modalInfo.url} className="w-[55%] h-full object-cover" />
 
-                    <div className='flex flex-col gap-6 px-6 overflow-auto'>
+                    <div className='flex flex-col gap-6 pl-6 pr-3 overflow-auto'>
                         <h2 className='text-xl border-b pb-4 border-b-gray-400'>{modalInfo.descricao}</h2>
 
                         <div className='flex items-center justify-start'>
@@ -49,10 +47,10 @@ export function Modal(){
                             <p className='text-green-600 py-3'>{`${modalInfo.estoque} em estoque`}</p>
                         </div>
 
-                        <div className='flex items-center justify-start gap-4'>
+                        <div className='flex items-center justify-start gap-2'>
                             <span>Quant: </span>
                             <input className='border border-zinc-400 focus:border-zinc-400 w-12 text-center py-2 px-2 rounded-md' pattern='[0-9]' type="number" name="quant" defaultValue={1} max={999} />
-                            <button className='py-2 px-5 bg-blue-700 text-white rounded-md'>Adicionar ao carrinho</button>
+                            <button className='py-3 px-2 bg-blue-700 text-white text-xs rounded-md'>Adicionar ao carrinho</button>
                         </div>
                     </div>
                 </section>
