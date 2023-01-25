bfd537232224b7a32bba7901f137108513b29d50
Alerrando <alerrando2@gmail.com>
2023-01-25 16:33:08 -0300

    starting the filter component

--- src/components/Produtos/Main/Filtro/index.tsx
+++ src/components/Produtos/Main/Filtro/index.tsx
@@ -2,8 +2,10 @@ import React from 'react';
 
 export function Filtro(props) {
     return (
-        <section className='w-auto h-full'>
-            
+        <section className='w-auto h-full bg-white'>
+            <div className='w-80 h-full'>
+
+            </div>
         </section>
     );
 }
--- src/components/Produtos/Main/index.tsx
+++ src/components/Produtos/Main/index.tsx
@@ -3,8 +3,10 @@ import { Filtro } from './Filtro';
 
 export function Main(props) {
     return (
-        <main className='w-full pt-[9%] mx-auto'>
-            <Filtro />
+        <main className='w-full h-screen  pt-[12%] mx-auto bg-[#F1F3F8]'>
+            <div className='flex items-center justify-between w-full h-full'>
+                <Filtro />
+            </div>
         </main>
     );
 }
