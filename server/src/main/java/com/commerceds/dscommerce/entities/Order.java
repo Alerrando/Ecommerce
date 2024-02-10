e24ec57aa7e050bbf1a4fe05871fc3d06f3785a1
Alerrando <alerrando2@gmail.com>
2024-02-10 18:55:45 -0300

    feat: add order and order_item_pk

--- server/src/main/java/com/commerceds/dscommerce/entities/Order.java
+++ server/src/main/java/com/commerceds/dscommerce/entities/Order.java
@@ -3,6 +3,9 @@ package com.commerceds.dscommerce.entities;
 import jakarta.persistence.*;
 
 import java.time.Instant;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 @Entity
 @Table(name = "tb_order")
@@ -19,6 +22,8 @@ public class Order {
     private User client;
     @OneToOne(mappedBy = "order", cascade = CascadeType.ALL)
     private Payment payment;
+    @OneToMany(mappedBy = "id.order")
+    private Set<OrderItem> items = new HashSet<>();
 
     public Order(){
     }
@@ -69,5 +74,13 @@ public class Order {
 
     public void setPayment(Payment payment) {
         this.payment = payment;
+    }
+
+    public Set<OrderItem> getItems() {
+        return items;
+    }
+
+    public List<Product> getProducts(){
+        return items.stream().map(x -> x.getProduct()).toList();
     }
 }
--- server/src/main/java/com/commerceds/dscommerce/entities/Product.java
+++ server/src/main/java/com/commerceds/dscommerce/entities/Product.java
@@ -3,6 +3,7 @@ package com.commerceds.dscommerce.entities;
 import jakarta.persistence.*;
 
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 @Entity
@@ -21,7 +22,8 @@ public class Product {
             joinColumns = @JoinColumn(name = "product_id"),
             inverseJoinColumns = @JoinColumn(name = "category_id"))
     private Set<Category> categories = new HashSet<>();
-
+    @OneToMany(mappedBy = "id.product")
+    private Set<OrderItem> items = new HashSet<>();
     public Product (){
     }
 
@@ -75,5 +77,13 @@ public class Product {
 
     public Set<Category> getCategories() {
         return categories;
+    }
+
+    public Set<OrderItem> getItems() {
+        return items;
+    }
+
+    public List<Order> getOrders(){
+        return items.stream().map(x -> x.getOrder()).toList();
     }
 }
