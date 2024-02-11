7dc6ff9b0159b69df97286607b7fd6b1f5434919
Alerrando <alerrando2@gmail.com>
2024-02-11 10:57:06 -0300

    feat: add equals and hashCode to entities

--- server/src/main/java/com/commerceds/dscommerce/entities/OrderItem.java
+++ server/src/main/java/com/commerceds/dscommerce/entities/OrderItem.java
@@ -4,8 +4,11 @@ import jakarta.persistence.EmbeddedId;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
 
+import java.util.Objects;
+
 @Entity
 @Table(name = "tb_order_item")
+
 public class OrderItem {
     @EmbeddedId
     private OrderItemPK id = new OrderItemPK();
@@ -52,5 +55,20 @@ public class OrderItem {
 
     public void setPrice(Double price) {
         this.price = price;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        OrderItem orderItem = (OrderItem) o;
+
+        return Objects.equals(id, orderItem.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return id != null ? id.hashCode() : 0;
     }
 }
